93500bb99526654975f1081f38264ee83fd826a7
Edward Treece <edtreece@gmail.com>
2022-08-24 21:02:59 +0100

    Yelp MVP

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     @review.save
@@ -18,3 +19,11 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:rating, :content)
   end
 end
+
+# Issues with create method
+# 1. The below code doesn't work
+# if @review.save
+# redirect_to restaurant_path(@restaurant)
+# else
+# render :new, status: :unprocessable_entity
+# 2. Submitting a review doesn't create an instance of the review that shows on the page
